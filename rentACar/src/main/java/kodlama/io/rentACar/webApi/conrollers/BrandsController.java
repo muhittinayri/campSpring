a627c8deb73e1c82ed1dbcc81061c159f6bab71c
muhittinayri <ayri.muhittin@gmail.com>
2023-02-23 22:47:55 +0300

    2023 - Engin Demiroğ

---
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/conrollers/BrandsController.java
@@ -0,0 +1,51 @@
+package kodlama.io.rentACar.webApi.conrollers;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/brands")
+@AllArgsConstructor
+public class BrandsController {
+    private BrandService brandService;
+
+
+    //@GetMapping("/getall")
+    @GetMapping()
+    public List<GetAllBrandsResponse> getAll(){
+        return brandService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public GetByIdBrandResponse getById(@PathVariable int id){
+        return brandService.getById(id);
+    }
+
+    //@PostMapping("/add")
+    @PostMapping()
+    @ResponseStatus(code=HttpStatus.CREATED)
+    public void add(CreateBrandRequest createBrandRequest){
+        this.brandService.add(createBrandRequest);
+    }
+
+    @PutMapping
+    public void update(@RequestBody() UpdateBrandRequest updateBrandRequest){
+        this.brandService.update(updateBrandRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        this.brandService.delete(id);
+    }
+
+
+}
